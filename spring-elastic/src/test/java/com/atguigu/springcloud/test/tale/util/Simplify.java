fa6c79129dd5a84ae42144ba83c2ce346d35bc4a
smallorange <172577882@qq.com>
2023-08-30 20:31:07 +0800

    新增 simplify 简化多边形方法2

--- spring-elastic/src/test/java/com/atguigu/springcloud/test/tale/util/Simplify.java
+++ spring-elastic/src/test/java/com/atguigu/springcloud/test/tale/util/Simplify.java
@@ -2,6 +2,7 @@ package com.atguigu.springcloud.test.tale.util;
 
 import com.atguigu.springcloud.test.tale.shape.Point;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /*
@@ -21,6 +22,79 @@ public final class Simplify {
 
     public static List<Point> simplify(List<Point> points, int tolerance, boolean highestQuality) {
         return null;
+    }
+
+    // square distance between 2 points
+    private static double getSqDist(Point p1, Point p2) {
+        double dx = p1.getX() - p2.getX(), dy = p1.getY() - p2.getY();
+
+        return dx * dx + dy * dy;
+    }
+
+    // square distance from a point to a segment
+    private static double getSqSegDist(Point p, Point p1, Point p2) {
+        double x = p1.getX(), y = p1.getY(), dx = p2.getX() - x, dy = p2.getY() - y;
+
+        if (dx != 0 || dy != 0) {
+            double t = ((p.getX() - x) * dx + (p.getY() - y) * dy) / (dx * dx + dy * dy);
+
+            if (t > 1) {
+                x = p2.getX();
+                y = p2.getY();
+            } else if (t > 0) {
+                x += dx * t;
+                y += dy * t;
+            }
+        }
+
+        dx = p.getX() - x;
+        dy = p.getY() - y;
+
+        return dx * dx + dy * dy;
+    }
+
+    // basic distance-based simplification
+    private static List<Point> simplifyRadialDist(List<Point> points, int sqTolerance) {
+        Point prevPoint = points.get(0), point = null;
+        List<Point> newPoints = new ArrayList<>(points.size());
+        newPoints.add(prevPoint);
+
+        for (int i = 1, len = points.size(); i < len; i++) {
+            point = points.get(i);
+
+            if (getSqDist(point, prevPoint) > sqTolerance) {
+                newPoints.add(point);
+                prevPoint = point;
+            }
+        }
+
+        if (point != null && prevPoint != point) {
+            newPoints.add(point);
+        }
+
+        return newPoints;
+    }
+
+    private static void simplifyDPStep(List<Point> points, int first, int last, int sqTolerance, List<Point> simplified) {
+        int index = 0;
+        double maxSqDist = sqTolerance;
+
+        for (int i = first + 1; i < last; i++) {
+            double sqDist = getSqSegDist(points.get(i), points.get(first), points.get(last));
+
+            if (sqDist > maxSqDist) {
+                index = i;
+                maxSqDist = sqDist;
+            }
+        }
+
+        if (maxSqDist > sqTolerance) {
+            if (index - first > 1)
+                simplifyDPStep(points, first, index, sqTolerance, simplified);
+            simplified.add(points.get(index));
+            if (last - index > 1)
+                simplifyDPStep(points, index, last, sqTolerance, simplified);
+        }
     }
 
 }
