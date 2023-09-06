93669dc7cb479bb91a2e9bb355d58ee2278acb66
chengang <172577882@qq.com>
2023-09-06 23:27:17 +0800

    新增 buffer 计算缓冲区(辐射区) 部分代码2

--- spring-elastic/README.md
+++ spring-elastic/README.md
@@ -76,3 +76,24 @@ elasticsearch-setup-passwords interactive
 
 [几何计算-基于Turf.js实现多边形的拆分及合并](https://zhuanlan.zhihu.com/p/389395519)
 
+[GeoJSON三分钟入门教程](https://zhuanlan.zhihu.com/p/539689986)
+
+### d3-geo
+
+`d3-geo`库, 是`d3`工具包提供一个地理位置计算及转换操作库，主要包括了：
+
+`Projections`:投影器，将`GEOJSON`数据投射到平面上显示出来。
+
+`Path`：对`geojson`里面的`feature`进行计算，生成路径，进行测量,裁切, 等操作。
+
+[d3-geo](https://github.com/d3/d3-geo/blob/v1.7.1/index.js)
+
+#### jts
+
+常用可以用于`GIS`数据处理和空间计算的`java`包有`geotool`和`jts`。相对来说，`geotool`功能更全面，还可以用于数据转换、瓦片地图发布、栅格影像分析等，`jts`只能进行基本的数据处理和空间计算。但大多数情况下`jts`就完全够用了。
+
+[GIS算法：JAVA拓扑套件JTS](https://blog.csdn.net/qq_43842093/article/details/127255054)
+
+[浅谈浏览器端 WebGIS 开发可能会用到的、提升效率的 js 库](https://www.w3xue.com/exp/article/20232/81557.html)
+
+[geotool的官网](https://www.geotools.org/)
--- spring-elastic/src/test/java/com/atguigu/springcloud/test/tale/util/BufferHelper.java
+++ spring-elastic/src/test/java/com/atguigu/springcloud/test/tale/util/BufferHelper.java
@@ -3,13 +3,16 @@ package com.atguigu.springcloud.test.tale.util;
 import com.atguigu.springcloud.test.tale.TaleMeasurement;
 import com.atguigu.springcloud.test.tale.TaleMeta;
 import com.atguigu.springcloud.test.tale.enums.Units;
-import com.atguigu.springcloud.test.tale.shape.Geometry;
-import com.atguigu.springcloud.test.tale.shape.GeometryCollection;
-import com.atguigu.springcloud.test.tale.shape.GeometryType;
-import com.atguigu.springcloud.test.tale.shape.Point;
+import com.atguigu.springcloud.test.tale.exception.TaleException;
+import com.atguigu.springcloud.test.tale.shape.*;
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.geom.LineString;
+import com.vividsolutions.jts.operation.buffer.BufferOp;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public final class BufferHelper {
 
@@ -43,17 +46,179 @@ public final class BufferHelper {
         // Project GeoJSON to Azimuthal Equidistant projection (convert to Meters)
 
         // JSTS buffer operation
+        double distance = TaleHelper.radiansToLength(TaleHelper.lengthToRadians(radius, units), Units.METERS);
+
+        // 这里可以直接做一层中转，以后可以实现GeoJSON，就不需要这么麻烦咯。。。
+        com.vividsolutions.jts.geom.Geometry buffered = BufferOp.bufferOp(transformation(geometry), distance, steps);
 
         // Detect if empty geometries
+        if (coordsIsNaN(buffered.getCoordinates())) {
+            return null;
+        }
 
         // Unproject coordinates (convert to Degrees)
+        return unTransformation(buffered);
+    }
 
+    private static Geometry unTransformation(com.vividsolutions.jts.geom.Geometry geometry) {
         return null;
     }
 
+    /**
+     * 将 Geometry 转成对应的 JTS的Geometry
+     *
+     * @param geometry 要转换的组件
+     * @return JTS Geometry 组件
+     */
+    private static com.vividsolutions.jts.geom.Geometry transformation(Geometry geometry) {
+        GeometryFactory geometryFactory = new GeometryFactory();
+
+        GeometryType type = geometry.type();
+        switch (type) {
+            case POINT: {
+                Point p = Point.point(geometry);
+                return geometryFactory.createPoint(new Coordinate(p.getX(), p.getY()));
+            }
+            case MULTI_POINT: {
+                MultiPoint multiPoint = MultiPoint.multiPoint(geometry);
+                return geometryFactory.createMultiPoint(multiPoint.coordinates().stream().map(p -> new Coordinate(p.getX(), p.getY())).toArray(Coordinate[]::new));
+            }
+            case LINE: {
+                Line line = Line.line(geometry);
+                return geometryFactory.createLineString(line.coordinates().stream().map(p -> new Coordinate(p.getX(), p.getY())).toArray(Coordinate[]::new));
+            }
+            case MULTI_LINE: {
+                MultiLine multiLine = MultiLine.multiLine(geometry);
+                return geometryFactory.createMultiLineString(
+                        multiLine.coordinates()
+                                .stream()
+                                .map(pointList -> geometryFactory.createLineString(pointList.stream().map(p -> new Coordinate(p.getX(), p.getY())).toArray(Coordinate[]::new)))
+                                .toArray(LineString[]::new)
+                );
+            }
+            case POLYGON: {
+                Polygon polygon = Polygon.polygon(geometry);
+                return geometryFactory.createPolygon(polygon.coordinates().stream().map(p -> new Coordinate(p.getX(), p.getY())).toArray(Coordinate[]::new));
+            }
+            case MULTI_POLYGON: {
+                MultiPolygon multiPolygon = MultiPolygon.multiPolygon(geometry);
+                return geometryFactory.createMultiPolygon(
+                        multiPolygon.coordinates()
+                                .stream()
+                                .map(pointList -> geometryFactory.createPolygon(pointList.stream().map(p -> new Coordinate(p.getX(), p.getY())).toArray(Coordinate[]::new)))
+                                .toArray(com.vividsolutions.jts.geom.Polygon[]::new)
+                );
+            }
+            default:
+                throw new TaleException(type + " geometry not supported");
+        }
+    }
+
+    public static void main(String[] args) {
+        Point p = Point.fromLngLat(1, 2);
+        double distance = TaleHelper.radiansToLength(TaleHelper.lengthToRadians(500, Units.KILOMETERS), Units.METERS);
+
+        GeometryFactory geometryFactory = new GeometryFactory();
+        com.vividsolutions.jts.geom.Point v = geometryFactory.createPoint(new Coordinate(1, 2));
+        com.vividsolutions.jts.geom.Geometry vv = BufferOp.bufferOp(v, distance, 8);
+        System.out.println(vv.toText());
+    }
+
+    /**
+     * 判断坐标中的值是否是 NaN
+     *
+     * @param coords 坐标集合
+     * @return 如果 NaN 存在返回true，否则false
+     */
+    private static boolean coordsIsNaN(Coordinate[] coords) {
+        if (coords == null || coords.length == 0) {
+            return false;
+        }
+        for (Coordinate coord : coords) {
+            if (Double.isNaN(coord.x)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 投影坐标
+     *
+     * @param coords 要计算的坐标
+     * @param proj   D3地理投影
+     * @return 投影后的坐标
+     */
+    private static double[] projectCoords(Point coords, String proj) {
+        return null;//proj(coords);
+    }
+
+    /**
+     * 投影坐标
+     *
+     * @param coords 要计算的坐标集合
+     * @param proj   D3地理投影
+     * @return 投影后的坐标集合
+     */
+    private static List<double[]> projectCoords(List<Point> coords, String proj) {
+        return coords.stream().map(p -> projectCoords(p, proj)).collect(Collectors.toList());
+    }
+
+    /**
+     * 投影坐标
+     *
+     * @param coords 要计算的坐标集合
+     * @param proj   D3地理投影
+     * @return 投影后的坐标集合
+     */
+    private static List<List<double[]>> projectMultiCoords(List<List<Point>> coords, String proj) {
+        return coords.stream().map(plist -> projectCoords(plist, proj)).collect(Collectors.toList());
+    }
+
+    /**
+     * 将坐标取消投影
+     *
+     * @param coords 要取消的坐标
+     * @param proj   D3地理投影
+     * @return 取消后的坐标
+     */
+    private static double[] unprojectCoords(Point coords, String proj) {
+        return null;//proj.invert(coords);
+    }
+
+    /**
+     * 将坐标取消投影
+     *
+     * @param coords 要取消的坐标集合
+     * @param proj   D3地理投影
+     * @return 取消后的坐标集合
+     */
+    private static List<double[]> unprojectCoords(List<Point> coords, String proj) {
+        return coords.stream().map(p -> unprojectCoords(p, proj)).collect(Collectors.toList());
+    }
+
+    /**
+     * 将坐标取消投影
+     *
+     * @param coords 要取消的坐标集合
+     * @param proj   D3地理投影
+     * @return 取消后的坐标集合
+     */
+    private static List<List<double[]>> unprojectMultiCoords(List<List<Point>> coords, String proj) {
+        return coords.stream().map(plist -> unprojectCoords(plist, proj)).collect(Collectors.toList());
+    }
+
+    /**
+     * 定义方位等距投影
+     *
+     * @param geometry 图形组件
+     * @return D3 地理方位等距投影
+     */
     private static double defineProjection(Geometry geometry) {
         Point coords = TaleMeasurement.center(geometry);
+        double[] rotation = new double[]{-coords.getX(), -coords.getY()};
 
+        return 0;//geoAzimuthalEquidistant().rotate(rotation).scale(TaleHelper.EARTH_RADIUS);
     }
 
 }
