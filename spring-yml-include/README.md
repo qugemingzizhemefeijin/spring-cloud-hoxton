84e5e516c8bc72243da0d050a6e399c0627925d4
smallorange <172577882@qq.com>
2024-09-09 17:45:53 +0800

    新增 tesseract 图片文字识别

--- spring-yml-include/README.md
+++ spring-yml-include/README.md
@@ -1,1 +1,22 @@
-springboot中spring.profiles.include使用
+### tesseract
+
+[Introduction](https://tesseract-ocr.github.io/tessdoc/Installation.html)
+
+[Windows Tesseract at UB Mannheim 下载地址](https://github.com/UB-Mannheim/tesseract/wiki)
+
+[Linux 下载地址](https://tesseract-ocr.github.io/tessdoc/Downloads.html)
+
+[Traineddata Files for Version 4.00 +](https://tesseract-ocr.github.io/tessdoc/Data-Files.html)
+
+[tesseract github](https://github.com/tesseract-ocr/tesseract)
+
+[tessdata github](https://github.com/tesseract-ocr/tessdata)
+
+[Tesseract-OCR 安装、中文识别与训练字库](https://www.jianshu.com/p/3326c7216696)
+
+配置环境变量
+
+```
+TESSDATA_PREFIX = D:\Program Files\Tesseract-OCR
+PATH = PATH & %tessdata%\tessdata
+```
---
+++ spring-yml-include/src/main/java/com/atguigu/springcloud/other/tess4j/OCRExample.java
@@ -0,0 +1,36 @@
+package com.atguigu.springcloud.other.tess4j;
+
+import net.sourceforge.tess4j.ITesseract;
+import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.TesseractException;
+
+import java.io.File;
+
+public class OCRExample {
+
+    public static void main(String[] args) {
+        // 创建 Tesseract 实例
+        ITesseract tesseract = new Tesseract();
+
+        // 设置 Tesseract 的数据目录路径
+        tesseract.setDatapath("D:\\Program Files\\Tesseract-OCR\\tessdata"); // Tesseract 数据目录的实际路径
+
+        // 设置语言（例如英文），可以指定多个语言，用 "+" 连接（例如 "eng+chi_sim"）
+        tesseract.setLanguage("eng"); // 也可以使用其他语言代码
+
+        // 指定要识别的图像文件
+        File imageFile = new File("C:\\Users\\a\\Desktop\\eng.png"); // 替换为你要识别的图像文件路径
+
+        try {
+            // 执行 OCR 识别
+            String result = tesseract.doOCR(imageFile);
+
+            // 输出识别结果
+            System.out.println("识别结果: " + result);
+
+        } catch (TesseractException e) {
+            System.err.println("识别失败: " + e.getMessage());
+        }
+    }
+
+}
